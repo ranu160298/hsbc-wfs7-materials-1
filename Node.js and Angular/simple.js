aca3dfda839ae8b709a9dae3376b4966a71472a9
Kishor <kishor1124@gmail.com>
2020-10-14 17:27:13 +0530

    angular demos

---
+++ Node.js and Angular/simple.js
@@ -0,0 +1,8 @@
+var items = ["Alex", "Bruce", "Charles", "David"];
+
+items.forEach(function(i) {
+    console.log('i = '+i);
+});
+console.log('-----------------');
+
+items.forEach(i => console.log('i = '+i));
