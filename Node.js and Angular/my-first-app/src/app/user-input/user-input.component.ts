c6ed0ea5a0125cc35be57da9bdb5298e220eb45c
Kishor <kishor1124@gmail.com>
2020-10-15 12:21:12 +0530

    event binding demo

---
+++ Node.js and Angular/my-first-app/src/app/user-input/user-input.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-input',
+  templateUrl: './user-input.component.html',
+  styleUrls: ['./user-input.component.css']
+})
+export class UserInputComponent {
+
+  username  = undefined;
+  password = undefined;
+
+  handleButton(u, p) {
+    this.username = u;
+    this.password = p;
+    console.log('handleButton() called');
+  }
+  handleKeyInput(u) {
+    console.log('handleInput() called: '+u)
+  }
+
+}
