2fa5471913943c02311172e659d014bc25533e04
Kishor <kishor1124@gmail.com>
2020-10-15 15:24:33 +0530

    @input binding demo

--- Node.js and Angular/my-first-app/src/app/app.module.ts
+++ Node.js and Angular/my-first-app/src/app/app.module.ts
@@ -6,14 +6,20 @@ import { AppComponent } from './app.component';
 import { XyzComponent } from './xyz/xyz.component';
 import { ProfileDisplayComponent } from './profile-display/profile-display.component';
 import { UserInputComponent } from './user-input/user-input.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
+import { NamesItemComponent } from './names-item/names-item.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     XyzComponent,
     ProfileDisplayComponent,
-    UserInputComponent
+    UserInputComponent,
+    ParentComponent,
+    ChildComponent,
+    NamesItemComponent
   ],
   imports: [
     BrowserModule, FormsModule
---
+++ Node.js and Angular/my-first-app/src/app/names-item/names-item.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NamesItemComponent } from './names-item.component';
+
+describe('NamesItemComponent', () => {
+  let component: NamesItemComponent;
+  let fixture: ComponentFixture<NamesItemComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NamesItemComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NamesItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
