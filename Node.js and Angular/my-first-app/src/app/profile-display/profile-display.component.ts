0e9d3d548883665f6cd72feeca573a0ab800d72b
Kishor <kishor1124@gmail.com>
2020-10-16 12:28:37 +0530

    angular service demo

--- Node.js and Angular/my-first-app/src/app/profile-display/profile-display.component.ts
+++ Node.js and Angular/my-first-app/src/app/profile-display/profile-display.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { USER } from '../mock'
+import {AppServiceService} from '../app-service.service'
 @Component({
   selector: 'app-profile-display',
   templateUrl: './profile-display.component.html',
   styleUrls: ['./profile-display.component.css']
 })
 export class ProfileDisplayComponent  {
-  profile : any = undefined;
-  salary = 2500;
-  constructor() {
-    this.profile = USER;
+ 
+  userCollection : any = undefined;
+  constructor(private service: AppServiceService) {}
+
+  handleClick() {
+    this.userCollection = this.service.fetchUsers();
   }
 }
